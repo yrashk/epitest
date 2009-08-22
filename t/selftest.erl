d4f5ce171babdbb6e6e04bbf24f2ef1735739299
Yurii Rashkovskii <yrashk@gmail.com>
2009-08-21 17:29:15 -0700

    some progress on selftest

--- t/selftest.erl
+++ t/selftest.erl
@@ -2,6 +2,110 @@
 -include_lib("epitest/include/epitest.hrl").
 
 test("Simple test with no dependencies, no function") ->
-    [].
+    [];
+
+test("Simple test with no dependencies") ->
+    [
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+
+test("Simple test with a single r dependency") ->
+    [
+     {r, ["Simple test with no dependencies"]},
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+
+test("Simple test that fails") ->
+    [{f,
+      fun () ->
+	      io:format("[next failure is ok]"),
+	      5 = apply(fun erlang:'+'/2, [2,2])
+      end}];
+
+test("Another simple test that fails") ->
+    [{f,
+      fun () ->
+	      io:format("[next failure is ok]"),
+	      5 = apply(fun erlang:'+'/2, [2,2])
+      end}];
+
+test("Simple test with a single r dependency that should never be reached") ->
+    [
+     {r, ["Simple test that fails"]},
+     {f,
+      fun () ->
+	      io:format("[YOU REACHED TEST THAT YOU SHOULDN'T #1]"),
+	      ok
+      end}
+    ];
+
+test("Simple test with a single fr dependency") ->
+    [
+     {fr, ["Simple test that fails"]},
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+
+test("Simple test with a single ir dependency") ->
+    [
+     {fr, ["Simple test that fails"]},
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+
+test("Simple test with a multiple r dependency") ->
+    [
+     {r, ["Simple test with no dependencies, no function","Simple test with no dependencies"]},
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+    
+test("Simple test with a multiple r dependency that should never be reached") ->
+    [
+     {r, ["Simple test that fails","Simple test with no dependencies"]},
+     {f,
+      fun () ->
+	      io:format("[YOU REACHED TEST THAT YOU SHOULDN'T #3]")
+      end}
+    ];
+
+test("Simple test with a multiple fr dependency") ->
+    [
+     {fr, ["Simple test that fails","Another simple test that fails"]},
+     {f,
+      fun () ->
+	      ok
+      end}
+    ];
+    
+
+test("Simple test with a multiple fr dependency that should never be reached") ->
+    [
+     {fr, ["Simple test that fails","Simple test with no dependencies"]},
+     {f,
+      fun () ->
+	      io:format("[YOU REACHED TEST THAT YOU SHOULDN'T #3]")
+      end}
+    ];
 
 
+test("Another simple test with a multiple fr dependency that should never be reached") ->
+    [
+     {fr, ["Simple test with no dependencies, no function","Simple test with no dependencies"]},
+     {f,
+      fun () ->
+	      io:format("[YOU REACHED TEST THAT YOU SHOULDN'T #4]")
+      end}
+    ].
