70c033a3725c47cb671866561e1f7ca39ad6df4e
Yurii Rashkovskii <yrashk@gmail.com>
2010-04-05 20:50:22 -0700

    A little bit nicer failure reasons rendering (console logger)

--- src/epitest_console_logger.erl
+++ src/epitest_console_logger.erl
@@ -7,7 +7,9 @@
 -export([init/1, handle_event/2, handle_call/2, 
          handle_info/2, terminate/2, code_change/3]).
 
--record(state, { passed = 0, failed = 0, pending = 0, unreachable = 0 }).
+-record(state, { passed = 0, failed = 0, pending = 0, unreachable = 0,
+                 stacktraces = []
+               }).
 
 init([]) ->
     {ok, #state{}}.
@@ -26,15 +28,24 @@ handle_event(#epistate{ state = {failed, {failed_requirement, Type, FRTest}}, te
     #test{ loc = Loc } = Test,
     #test{ loc = FRLoc } = FRTest,
     io:format("\e[36m[UNREAC]  ~s (~s): expected \"~s\" (~s) to be a ~p\e[0m~n", [format_name(Test), format_loc(Loc), format_name(FRTest), format_loc(FRLoc), Type]),
-    {ok, State#state{ unreachable = State#state.unreachable + 1} };
+    {ok, State#state{ unreachable = State#state.unreachable + 1 } };
 
-handle_event(#epistate{ state = {failed, Res}, test = Test }, State) ->
+handle_event(#epistate{ state = {failed, {Res, Stacktrace}}, test = Test }, #state{ stacktraces = Stacktraces } = State) ->
     #test{ loc = Loc } = Test,
-    io:format("\e[31m[FAILED] \e[32m \e[37m~s\e[32m (~s):\e[31m~200p\e[0m~n", [format_name(Test), format_loc(Loc), Res]),
-    {ok, State#state{ failed = State#state.failed + 1} };
+    io:format("\e[31m[FAILED] \e[32m \e[37m~s\e[32m (~s):\e[31m ~s (stacktrace #~w)\e[0m~n", [format_name(Test), format_loc(Loc), format_reason(Res), length(Stacktraces) + 1]),
+    {ok, State#state{ failed = State#state.failed + 1,
+                      stacktraces = Stacktraces ++ [Stacktrace]
+                    } };
 
 handle_event({finished, _Plan}, State) ->
-    io:format("\e[32mPassed: ~w \e[31mFailed: ~w \e[33mPending: ~w \e[36mUnreachable: ~w\e[0m~n",[State#state.passed, State#state.failed, State#state.pending, State#state.unreachable]),
+    io:format("~n\e[4m\e[32mPassed: ~w \e[31mFailed: ~w \e[33mPending: ~w \e[36mUnreachable: ~w\e[24m\e[0m~n",[State#state.passed, State#state.failed, State#state.pending, State#state.unreachable]),
+    %% Dump stacktraces
+    io:format("~n\e[4m\e[31mStracktraces:\e[24m~n~n"),
+    lists:foldl(fun (Stacktrace, Index) ->
+                        io:format("\e[37m#~w)\e[31m ~p~n~n", [Index, Stacktrace]),
+                        Index + 1
+                end, 1, State#state.stacktraces),
+    io:format("~n\e[0m"),
     {ok, State};
 
 handle_event(_Event, State) ->
@@ -63,3 +74,8 @@ format_loc({module, {Module, _}, Line}) ->
     io_lib:format("~w.erl:~p",[Module, Line]);
 format_loc(dynamic) ->
     io_lib:format("dynamic").
+
+format_reason(negative) ->
+    io_lib:format("This test should have failed", []);
+format_reason(Reason) ->
+    io_lib:format("~p",[Reason]).
