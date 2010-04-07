aa2e76ff6029d7655fdec3684f31e6585fdb8eb8
Yurii Rashkovskii <yrashk@gmail.com>
2010-04-06 21:16:44 -0700

    Another approach to negative tests, just change the result, no exception handling trickery

--- src/epitest_ph_negative.erl
+++ src/epitest_ph_negative.erl
@@ -6,49 +6,21 @@
 init() ->
     {ok, undefined}.
 
-handle_call({normalize, #test{ descriptor = Descriptor0 } = Test}, _From, State) ->
-    Descriptor =
-        case lists:member(negative, Descriptor0) of
-            true ->
-                rewrite_functors(Test);
-            _ ->
-                Descriptor0
-        end,
-    {reply, {ok, Test#test{ descriptor = Descriptor }}, State};
+handle_call({{start,  #epistate{ id = ID, test_plan = Plan, worker = Worker }},
+             #test{ descriptor = Descriptor } = Test}, _From, State) ->
+    #epistate{ handlers_properties = Properties } = epitest_test_plan_server:lookup(Plan, ID),
+    Invert = proplists:get_value(negative, Descriptor, false),
+    Result = proplists:get_value(functor_result, Properties),
+    gen_fsm:send_event(Worker, result(Invert, Result)),
+    {reply, {ok, Test}, State};
 
 handle_call({_Message, Result}, _From, State) ->
     {reply, {ok, Result}, State}.
 
-rewrite_functors(#test{ descriptor = Descriptor }) ->
-    rewrite_functors(Descriptor);
-rewrite_functors([{functor, F}|Rest]) when is_function(F) ->
-    [{functor, negative_functor(F)}|rewrite_functors(Rest)];
-rewrite_functors([_Property|Rest]) ->
-    rewrite_functors(Rest);
-rewrite_functors([]) ->
-    [].
 
-negative_functor(F) when is_function(F, 0) ->
-    fun () ->
-            try 
-                F(),
-                epitest_helpers:fail(negative)
-            catch
-                error:negative ->
-                    erlang:error(negative);
-                error:_Reason -> 
-                    suppress
-            end
-    end;
-negative_functor(F) when is_function(F, 1) ->
-    fun (Epistate) ->
-            try
-                F(Epistate),
-                epitest_helpers:fail(negative)
-            catch
-                error:negative ->
-                    erlang:error(negative);
-                error:_Reason ->
-                    suppress
-            end
-    end.
+result(true, success) ->
+    {failure, negative};
+result(true, {failure, _}) ->
+    success;
+result(false, Result) ->
+    Result.
